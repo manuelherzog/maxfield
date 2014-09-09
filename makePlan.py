74081e053d02e194502ff3cd29745d7bb7ec45f8
Manuel Herzog <manuelherzog@gmail.com>
2014-09-09 20:48:42 +0200

    refactored to whitespace instead of tab

--- makePlan.py
+++ makePlan.py
@@ -44,172 +44,172 @@ from lib import maxfield,PlanPrinter,geometry,agentOrder
 import pickle
 
 def main():
-	args = docopt(__doc__)
+    args = docopt(__doc__)
 
-	# We will take many samples in an attempt to reduce number of keys to farm
-	# This is the number of samples to take since the last improvement
-	EXTRA_SAMPLES = 20
+    # We will take many samples in an attempt to reduce number of keys to farm
+    # This is the number of samples to take since the last improvement
+    EXTRA_SAMPLES = 20
 
-	np = geometry.np
+    np = geometry.np
 
-	#GREEN = 'g'
-	#BLUE  = 'b'
-	GREEN = '#3BF256' # Actual faction text colors in the app
-	BLUE  = '#2ABBFF'
-	#GREEN = (0.0 , 1.0 , 0.0 , 0.3)
-	#BLUE  = (0.0 , 0.0 , 1.0 , 0.3)
-	COLOR = GREEN
+    #GREEN = 'g'
+    #BLUE  = 'b'
+    GREEN = '#3BF256' # Actual faction text colors in the app
+    BLUE  = '#2ABBFF'
+    #GREEN = (0.0 , 1.0 , 0.0 , 0.3)
+    #BLUE  = (0.0 , 0.0 , 1.0 , 0.3)
+    COLOR = GREEN
 
-	if args['-b']:
-		COLOR = BLUE
+    if args['-b']:
+        COLOR = BLUE
 
-	output_directory = ''
-	if args['<output_directory>'] != None:
-		output_directory = args['<output_directory>']
-		if output_directory[-1] != '/':
-			output_directory += '/'
+    output_directory = ''
+    if args['<output_directory>'] != None:
+        output_directory = args['<output_directory>']
+        if output_directory[-1] != '/':
+            output_directory += '/'
 
-	output_file = 'lastPlan.pkl'
-	if args['<output_file>'] != None:
-		output_file = args['<output_file>']
-		if not output_file[-3:] == 'pkl':
-			print 'WARNING: output file should end in "pkl" or you cannot use it as input later'
+    output_file = 'lastPlan.pkl'
+    if args['<output_file>'] != None:
+        output_file = args['<output_file>']
+        if not output_file[-3:] == 'pkl':
+            print 'WARNING: output file should end in "pkl" or you cannot use it as input later'
 
-	nagents = int(args['-n'])
-	if nagents < 0:
-		print 'Numer of agents should be positive'
-		exit()
+    nagents = int(args['-n'])
+    if nagents < 0:
+        print 'Numer of agents should be positive'
+        exit()
 
-	input_file = args['<input_file>']
+    input_file = args['<input_file>']
 
-	if input_file[-3:] != 'pkl':
-		a = nx.DiGraph()
+    if input_file[-3:] != 'pkl':
+        a = nx.DiGraph()
 
-		locs = []
+        locs = []
 
-		i = 0
-		# each line should be id,name,lat,long,keys
-		with open(input_file,'r') as fin:
-			for line in fin:
-				parts = line.split(',')
+        i = 0
+        # each line should be id,name,lat,long,keys
+        with open(input_file,'r') as fin:
+            for line in fin:
+                parts = line.split(',')
 
-				if len(parts) < 3:
-					break
+                if len(parts) < 3:
+                    break
 
-				a.add_node(i)
-				a.node[i]['name'] = parts[0].strip()
+                a.add_node(i)
+                a.node[i]['name'] = parts[0].strip()
 
-				locs.append( np.array(parts[1:3],dtype=int) )
+                locs.append( np.array(parts[1:3],dtype=int) )
 
-				if len(parts) < 4:
-					a.node[i]['keys'] = 0
-				else:
-					a.node[i]['keys'] = int(parts[3])
+                if len(parts) < 4:
+                    a.node[i]['keys'] = 0
+                else:
+                    a.node[i]['keys'] = int(parts[3])
 
-				i += 1
+                i += 1
 
-		n = a.order() # number of nodes
+        n = a.order() # number of nodes
 
-		locs = np.array(locs,dtype=float)
+        locs = np.array(locs,dtype=float)
 
-		# This part assumes we're working with E6 latitude-longitude data
-		locs = geometry.e6LLtoRads(locs)
-		xyz  = geometry.radstoxyz(locs)
-		xy   = geometry.gnomonicProj(locs,xyz)
+        # This part assumes we're working with E6 latitude-longitude data
+        locs = geometry.e6LLtoRads(locs)
+        xyz  = geometry.radstoxyz(locs)
+        xy   = geometry.gnomonicProj(locs,xyz)
 
-		for i in xrange(n):
-			a.node[i]['geo'] = locs[i]
-			a.node[i]['xyz'] = xyz [i]
-			a.node[i]['xy' ] = xy  [i]
+        for i in xrange(n):
+            a.node[i]['geo'] = locs[i]
+            a.node[i]['xyz'] = xyz [i]
+            a.node[i]['xy' ] = xy  [i]
 
-		# EXTRA_SAMPLES attempts to get graph with few missing keys
-		# Try to minimuze TK + 2*MK where
-		#   TK is the total number of missing keys
-		#   MK is the maximum number of missing keys for any single portal
-		bestgraph = None
-		bestlack = np.inf
-		bestTK = np.inf
-		bestMK = np.inf
+        # EXTRA_SAMPLES attempts to get graph with few missing keys
+        # Try to minimuze TK + 2*MK where
+        #   TK is the total number of missing keys
+        #   MK is the maximum number of missing keys for any single portal
+        bestgraph = None
+        bestlack = np.inf
+        bestTK = np.inf
+        bestMK = np.inf
 
-		sinceImprove = 0
+        sinceImprove = 0
 
-		while sinceImprove<EXTRA_SAMPLES:
-			b = a.copy()
+        while sinceImprove<EXTRA_SAMPLES:
+            b = a.copy()
 
-			sinceImprove += 1
+            sinceImprove += 1
 
-			if not maxfield.maxFields(b):
-				print 'Randomization failure\nThe program may work if you try again. It is more likely to work if you remove some protals.'
-				continue
+            if not maxfield.maxFields(b):
+                print 'Randomization failure\nThe program may work if you try again. It is more likely to work if you remove some protals.'
+                continue
 
-			TK = 0
-			MK = 0
-			for j in xrange(n):
-				keylack = max(b.in_degree(j)-b.node[j]['keys'],0)
-				TK += keylack
-				if keylack > MK:
-					MK = keylack
-			
-			weightedlack = TK+2*MK
+            TK = 0
+            MK = 0
+            for j in xrange(n):
+                keylack = max(b.in_degree(j)-b.node[j]['keys'],0)
+                TK += keylack
+                if keylack > MK:
+                    MK = keylack
+            
+            weightedlack = TK+2*MK
 
-			if weightedlack < bestlack:
-				sinceImprove = 0
-				print 'IMPROVEMENT:\n\ttotal: %s\n\tmax:   %s\n\tweighted: %s'%\
-					   (TK,MK,weightedlack)
-				bestgraph = b
-				bestlack  = weightedlack
-				bestTK  = TK
-				bestMK  = MK
-			else:
-				print 'this time:\n\ttotal: %s\n\tmax:   %s\n\tweighted: %s'%\
-					   (TK,MK,weightedlack)
+            if weightedlack < bestlack:
+                sinceImprove = 0
+                print 'IMPROVEMENT:\n\ttotal: %s\n\tmax:   %s\n\tweighted: %s'%\
+                       (TK,MK,weightedlack)
+                bestgraph = b
+                bestlack  = weightedlack
+                bestTK  = TK
+                bestMK  = MK
+            else:
+                print 'this time:\n\ttotal: %s\n\tmax:   %s\n\tweighted: %s'%\
+                       (TK,MK,weightedlack)
 
-			if weightedlack == 0:
-				print 'KEY PERFECTION'
-				bestlack  = weightedlack
-				bestTK  = TK
-				bestMK  = MK
-				break
+            if weightedlack == 0:
+                print 'KEY PERFECTION'
+                bestlack  = weightedlack
+                bestTK  = TK
+                bestMK  = MK
+                break
 
-			if all([ b.node[i]['keys'] >= b.in_degree(i) for i in xrange(n) ]):
-				print 'All keys used. Improvement impossible'
-				break
+            if all([ b.node[i]['keys'] >= b.in_degree(i) for i in xrange(n) ]):
+                print 'All keys used. Improvement impossible'
+                break
 
-			print '%s tries since improvement'%sinceImprove
+            print '%s tries since improvement'%sinceImprove
 
-		if bestgraph == None:
-			print 'EXITING RANDOMIZATION LOOP WITHOUT SOLUTION!'
-			print ''
-			exit()
+        if bestgraph == None:
+            print 'EXITING RANDOMIZATION LOOP WITHOUT SOLUTION!'
+            print ''
+            exit()
 
-		print 'Choosing plan requiring %s additional keys, max of %s from single portal'%(bestTK,bestMK)
+        print 'Choosing plan requiring %s additional keys, max of %s from single portal'%(bestTK,bestMK)
 
-		a = bestgraph
+        a = bestgraph
 
-		# Attach to each edge a list of fields that it completes
-		for t in a.triangulation:
-			t.markEdgesWithFields()
+        # Attach to each edge a list of fields that it completes
+        for t in a.triangulation:
+            t.markEdgesWithFields()
 
-		agentOrder.improveEdgeOrder(a)
+        agentOrder.improveEdgeOrder(a)
 
-		with open(output_directory+output_file,'w') as fout:
-			pickle.dump(a,fout)
-	else:
-		with open(input_file,'r') as fin:
-			a = pickle.load(fin)
-	#    agentOrder.improveEdgeOrder(a)
-	#    with open(output_directory+output_file,'w') as fout:
-	#        pickle.dump(a,fout)
+        with open(output_directory+output_file,'w') as fout:
+            pickle.dump(a,fout)
+    else:
+        with open(input_file,'r') as fin:
+            a = pickle.load(fin)
+    #    agentOrder.improveEdgeOrder(a)
+    #    with open(output_directory+output_file,'w') as fout:
+    #        pickle.dump(a,fout)
 
-	PP = PlanPrinter.PlanPrinter(a,output_directory,nagents,COLOR)
-	PP.keyPrep()
-	PP.agentKeys()
-	PP.planMap()
-	PP.agentLinks()
+    PP = PlanPrinter.PlanPrinter(a,output_directory,nagents,COLOR)
+    PP.keyPrep()
+    PP.agentKeys()
+    PP.planMap()
+    PP.agentLinks()
 
-	# These make step-by-step instructional images
-	#PP.animate()
-	#PP.split3instruct()
+    # These make step-by-step instructional images
+    #PP.animate()
+    #PP.split3instruct()
 
 if __name__ == "__main__":
-	sys.exit(main())
+    sys.exit(main())
